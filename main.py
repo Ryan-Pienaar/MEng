e9ffa7c89006f40c08c76f3afb296eb9f8bd5dd5
Tempest <ryanpienaar46@gmail.com>
2023-04-09 15:06:19 +0200

    Initial commit. Data struccture setup.

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+dataframe1 = pd.read_excel("Params_20210731.V2.xlsx")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(dataframe1)
+
+
