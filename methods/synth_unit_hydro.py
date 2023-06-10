d8c0435c87d23e52b1574e5347c2d6bde0cd9670
Tempest <ryanpienaar46@gmail.com>
2023-06-10 12:35:32 +0200

    added global variables

--- methods/synth_unit_hydro.py
+++ methods/synth_unit_hydro.py
@@ -1,2 +1,38 @@
+
+
+# ----- GLOBAL VARIABLES -----
+#TODO: Correct how certain variables are obtained
+area = 6331
+MAP = 518
+veldtype_nr1 = 4
+veldtype_nr2 = 7
+veldtype_nr3 = 5
+veldtype_perc1 = 97.29
+veldtype_perc2 = 2.71
+veldtype_perc3 = 0
+LC = 113.05
+KU = 0.384
+L = 186.696
+SCH = 0.00131
+S = 0.04186
+TL = 37.666
+TC = 47.875
+IC = 582856.728
+QP = 64.557
+
+def design_rainfall_info():
+    temp = 0
+
+def peak_flow_adjust():
+    temp = 0
+
+def s_curve_lagging():
+    temp = 0
+
+if __name__ == "__main__":
+    print("test")
+
+
+
 def excecute():
     print("METHOD NOT IMPLEMENTED")
